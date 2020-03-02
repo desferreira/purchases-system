7d9a010a269f993e7c496ddae486d4303e6b4761
desferreira <diego.ferreira@justa.com.vc>
2020-03-02 20:38:33 -0300

    :tada: Primeiro commit
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

---
+++ src/test/java/org/diego/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diego.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
