678a15d5d620f1a065c8fe30f94cd609ae894d17
desferreira <diego.ferreira@justa.com.vc>
2020-03-03 12:26:20 -0300

    :sparkles: Cria repository e service para a classe User
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.services;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+}
