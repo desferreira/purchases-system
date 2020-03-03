e673d3fe0f3d7eec32fd4cb05479d5773d7de64d
desferreira <diego.ferreira@justa.com.vc>
2020-03-02 21:01:32 -0300

    :hammer: Configura para primeira rodada
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,13 +1,13 @@
-package org.diego.springapp;
+package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringappApplication {
+public class CourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringappApplication.class, args);
+		SpringApplication.run(CourseApplication.class, args);
 	}
 
 }
--- src/test/java/org/diego/springapp/SpringappApplicationTests.java
+++ src/test/java/org/diego/springapp/SpringappApplicationTests.java
@@ -1,6 +1,6 @@
 package org.diego.springapp;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
