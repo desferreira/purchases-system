7b07655f7b63685409938b4daad2b4b974b0389b
desferreira <diego.ferreira@justa.com.vc>
2020-03-03 08:03:48 -0300

    :sparkles: Implementa classe User e controlador REST básico
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.educandoweb.course.resources;
+
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource  {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Diego", "123", "123", "diego@hotmail.com");
+        return ResponseEntity.ok().body(u);
+
+    }
+
+
+
+}
