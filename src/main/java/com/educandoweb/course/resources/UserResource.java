36fa15c66e7b008dc93ddbefedf57f839d2ca1fe
desferreira <diego.ferreira@justa.com.vc>
2020-03-03 12:37:33 -0300

    :sparkles: Implementa getById no service e no controller
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,11 +1,13 @@
 package com.educandoweb.course.resources;
 
 
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,12 @@ public class UserResource {
     public ResponseEntity<List<User>> findAll() {
         List<User> users = service.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +16,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
     }
 
 }
