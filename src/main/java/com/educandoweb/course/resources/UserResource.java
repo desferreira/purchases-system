ff1ffb0923272b922e4977bf213607d2d068fcd7
desferreira <diego.ferreira@justa.com.vc>
2020-03-08 13:20:42 -0300

    :sparkles: Implementa o método insert() na classe User
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,6 +29,16 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping(value = "/insert")
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        User insertedUser = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(insertedUser.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(insertedUser);
+
     }
 
 
